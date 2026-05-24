de7959f3aa91eded7bcf14c2ca51d18e74ade473
Mateus-henrique17 <1205mateus@gmail.com>
2026-05-24 13:01:58 -0300

    feat: add edit button and global colors

--- js/services/save/saveTransaction.js
+++ js/services/save/saveTransaction.js
@@ -1,11 +1,11 @@
-export  async function saveTransaction(transaction) {
-  alert('Transaction saved!');
-
-  const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+export function saveTransaction(transaction) {
+  const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
   transactions.push(transaction);
 
-  localStorage.setItem('transactions', JSON.stringify(transactions));
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+  setTimeout(() => {
+    alert("Transaction saved!");
+  }, 50);
   return transactions;
-
 }
